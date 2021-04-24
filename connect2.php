7181c6181a2f92d15b3e8d1a502f2806a1ed7a10
YaminiNegi <yamininegiid@gmail.com>
2021-04-24 23:58:57 +0530

    Removed commented code

--- connect2.php
+++ connect2.php
@@ -1,12 +1,6 @@
 <?php
- // 	$title= $_POST['title'] ?? "";
-	// $author=$_POST['author']?? "";
-	// $description=$_POST['description']?? "";
-	// $image_url=$_POST['image_url']?? "";
-
-// Create connection
-		$conn = new mysqli('localhost', 'root', '','e_library');
-		$sql = "SELECT * FROM books ORDER BY id DESC";
-$result = $conn->query($sql);
-$conn->close();
+	$conn = new mysqli('localhost', 'root', '','e_library');
+	$sql = "SELECT * FROM books ORDER BY id DESC";
+	$result = $conn->query($sql);
+	$conn->close();
 ?>		
